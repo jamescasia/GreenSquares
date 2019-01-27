9c425b9bd63e713d61b5b4222ec1327c711e265c
unknown <wayperwayp@gmail.com>
2019-01-27 20:56:37 +0800

    updated opacity

--- GreenSquares.dart
+++ GreenSquares.dart
@@ -1,36 +1,44 @@
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
 
 // The Square Class
 class Squares {
   int percentToMax;
-  int date; 
-  Squares(int percentToMax,  int date) {
+  int date;
+  Squares(int percentToMax, int date) {
     this.percentToMax = percentToMax;
-    this.date = date; 
+    this.date = date;
   }
 }
 
 class GreenSquares extends StatefulWidget {
- final List<Squares> squareList;
- final int month;
- final Color squareColor ;
+  final List<Squares> squareList;
+  final int month;
+  final Color squareColor;
 
-  GreenSquares({Key key, @required this.squareList, @required this.month,   this.squareColor})
+  GreenSquares(
+      {Key key,
+      @required this.squareList,
+      @required this.month,
+      this.squareColor})
       : super(key: key);
 
-  
-
   @override
-  _GreenSquaresState createState() =>new _GreenSquaresState(squareList: this.squareList, month:this.month, squareColor: this.squareColor );
+  _GreenSquaresState createState() => new _GreenSquaresState(
+      squareList: this.squareList,
+      month: this.month,
+      squareColor: this.squareColor);
 }
 
 class _GreenSquaresState extends State<GreenSquares> {
   final List<Squares> squareList;
-  final Color squareColor ;
+  final Color squareColor;
   final int month;
 
   _GreenSquaresState(
-      {Key key, @required this.squareList , @required this.month, @required this.squareColor});
+      {Key key,
+      @required this.squareList,
+      @required this.month,
+      @required this.squareColor});
 
   @override
   Widget build(BuildContext context) {
@@ -40,46 +48,48 @@ class _GreenSquaresState extends State<GreenSquares> {
         children: <Widget>[
           Padding(
               padding: EdgeInsets.only(right: 5),
-              child: Container( 
+              child: Container(
                   height: 100,
                   width: 100,
-                  child:
-                   GridView.count(
+                  child: GridView.count(
                     crossAxisCount: 5,
-                    children:  
-                    
-                    List.generate(squareList.length-1, (index) {
+                    children: List.generate(squareList.length - 1, (index) {
                       // index += 1;
                       int work = squareList[index].percentToMax;
-                      List <String> mm = ["Jan", "Feb","Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
-                      var mp = mm[month-1];
-                      var dt = index+1;
+                      List<String> mm = [
+                        "Jan",
+                        "Feb",
+                        "Mar",
+                        "Apr",
+                        "May",
+                        "Jun",
+                        "Jul",
+                        "Aug",
+                        "Sep",
+                        "Oct",
+                        "Nov",
+                        "Dec"
+                      ];
+                      var mp = mm[month - 1];
+                      var dt = index + 1;
                       return Tooltip(
                           message: "$work actions on $mp $dt",
                           child: Padding(
                               padding: EdgeInsets.all(2),
-                              child:  Container(
-                                    // padding: EdgeInsets.all(10),
-                                    height: 2,
-                                    width: 2,
-                                    constraints:BoxConstraints.expand(width: 2, height: 2),
-                                    color: Colors.grey[200],
-                                    child: Opacity(
-                                      
-                                      // opacity: 1.0,
-                                      opacity: ( squareList[index].percentToMax)/6.0,
-                                      child: Container( 
+                              child: Container(
+                                height: 2,
+                                width: 2,
+                                constraints:
+                                    BoxConstraints.expand(width: 2, height: 2),
+                                color: Colors.grey[200],
+                                child: Container(
+                                  color: squareColor.withOpacity(
+                                      (squareList[index].percentToMax) / 6.0),
+                                ),
 
-                                        // height: 2,
-                                        // width: 2,
-                                        color: squareColor,
-                                      ),
-                                    )
-                                    // color: Colors.lightGreen[ (squareList[index].percentToMax==0) ? 50:(squareList[index].percentToMax>=5) ? (squareList[index].percentToMax+1)*100:(squareList[index].percentToMax)*100],
-                                  )));
-                    }
-                    
-                    ),
+                                // color: Colors.lightGreen[ (squareList[index].percentToMax==0) ? 50:(squareList[index].percentToMax>=5) ? (squareList[index].percentToMax+1)*100:(squareList[index].percentToMax)*100],
+                              )));
+                    }),
                   ))),
         ]);
   }
